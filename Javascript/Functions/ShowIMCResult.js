32251a612cfa33aa8c73bdc09c3159d528e270d8
Goncalves Mariana De Souza <n863658@gruposantander.com>
2023-06-13 11:30:15 -0300

    Take coments off and seletec imc result

--- Javascript/Functions/ShowIMCResult.js
+++ Javascript/Functions/ShowIMCResult.js
@@ -1,12 +1,16 @@
 import { calculateIMC } from "./CalculateIMC.js";
+// import { getUserInformation } from "./GetUserInformation.js";
 
 const sendInformationsButton = document.querySelector('#send-informations-button')
 
 function showIMCResult() {
-
+    
+    let resultField = document.querySelector('#imc-result')
+    
     let {resultIMC} = calculateIMC()
 
-    console.log(resultIMC);
+    resultField.innerHTML = resultIMC
+
 }
 
 sendInformationsButton.addEventListener('click', showIMCResult)
