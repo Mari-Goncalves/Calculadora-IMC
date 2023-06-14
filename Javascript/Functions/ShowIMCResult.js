7da75bbaf2695cbe97ebf9f3fa95e8f21c2bbfff
Goncalves Mariana De Souza <n863658@gruposantander.com>
2023-06-14 10:38:19 -0300

    Show imc function adjust

--- Javascript/Functions/ShowIMCResult.js
+++ Javascript/Functions/ShowIMCResult.js
@@ -1,16 +1,14 @@
-import { calculateIMC } from "./CalculateIMC.js";
-// import { getUserInformation } from "./GetUserInformation.js";
+import { calculateIMC } from "./CalculateIMC.js"
 
 const sendInformationsButton = document.querySelector('#send-informations-button')
 
 function showIMCResult() {
-    
-    let resultField = document.querySelector('#imc-result')
-    
+
     let {resultIMC} = calculateIMC()
 
+    let resultField = document.querySelector('#imc-result')
+    
     resultField.innerHTML = resultIMC
-
 }
 
 sendInformationsButton.addEventListener('click', showIMCResult)
