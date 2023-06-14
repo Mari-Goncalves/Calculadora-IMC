b66974e49c21054a50556f85cfd52ab2520ce1ad
Goncalves Mariana De Souza <n863658@gruposantander.com>
2023-06-14 10:42:45 -0300

    IMC Calculator done

--- Javascript/Functions/ShowIMCResult.js
+++ Javascript/Functions/ShowIMCResult.js
@@ -4,11 +4,11 @@ const sendInformationsButton = document.querySelector('#send-informations-button
 
 function showIMCResult() {
 
-    let {resultIMC} = calculateIMC()
+    let resultIMC = calculateIMC()
 
     let resultField = document.querySelector('#imc-result')
     
-    resultField.innerHTML = resultIMC
+    resultField.innerHTML = resultIMC ? `O seu IMC é ${resultIMC.toFixed(2)}` : ''
 }
 
 sendInformationsButton.addEventListener('click', showIMCResult)
